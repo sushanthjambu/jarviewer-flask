6395ca69f5af2fa50a60f691032284ad22079948
jambusushanth <jambusushanth@gmail.com>
2021-05-14 17:34:02 +0530

    Version1 Complete: Added Database functions and html file.

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, g, Response, url_for
 from botocore.exceptions import ClientError
 import boto3, os, logging, json, botocore
+import psycopg2
+from psycopg2 import pool
+from hashids import Hashids
+
 app = Flask(__name__)
+
+hashids = Hashids(min_length=5, salt=os.environ.get('AWS_SECRET_ACCESS_KEY'))
+
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 #wsgi_app = app.wsgi_app
-
 
 @app.route('/')
 def hello():
@@ -32,6 +38,59 @@ def presigned_request():
         logging.error(e)
         return None
     return json.dumps(presigned_post)
+
+def get_db():
+    if 'db' not in g:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        g.db = psycopg2.connect(DATABASE_URL, sslmode='require')
+        #g.db = psycopg2.connect(database='jarviewer', user='postgres', password='ceasar1@3')
+
+    return g.db
+
+@app.teardown_appcontext
+def close_conn(e):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+@app.route('/save_url', methods = ['POST'])
+def save_to_database():
+    db = get_db()
+    object_url = request.form['Location']
+    if object_url is None:
+        return Response("URL not received", status = 400)
+    cur = db.cursor()
+    try:
+        cur.execute("INSERT INTO objecturl (url) VALUES (%s) RETURNING id;", (object_url,))
+        db.commit()
+        row_id = cur.fetchone()[0]
+        hash_id = hashids.encode(row_id)
+        cur.close()
+        return request.url_root + url_for('display_ar', hash=hash_id)[1:]
+    except psycopg2.Error as e:
+        logging.error(e.pgerror)
+        cur.close()
+        db.rollback()
+        return Response("URL could not be saved to Database", status = 400)
+
+@app.route('/j_arviewer/<hash>', methods = ['GET'])
+def display_ar(hash):
+    row = hashids.decode(hash)
+    db = get_db()
+    cur = db.cursor()
+    try:
+        cur.execute("SELECT url FROM objecturl WHERE id = %s;", (row,))
+        url_source = cur.fetchone()[0]
+        cur.close()
+        return render_template("index.html", aws_url=url_source)
+    except psycopg2.Error as e:
+        logging.error(e.pgerror)
+        cur.close()
+        return Response("Object not found in Database", status = 404)
+
+    
+
+
 
 if __name__ == '__main__':
     HOST = os.environ.get('SERVER_HOST', 'localhost')
